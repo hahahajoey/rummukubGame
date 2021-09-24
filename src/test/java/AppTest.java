26037749393050e354be4919a237da362549a0fb
Chupeng Shen <shenchupeng0807@gmail.com>
2021-09-24 14:04:35 -0400

    TEST PLACE {QH QC QS}

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,11 +7,21 @@ public class AppTest {
 
     @DisplayName("P1 plays{JH QH KH}")
     @Test
-    void TestPlayPlaceJH_QH_KH() {
+    public void TestPlayPlaceJH_QH_KH() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"JH", "QH", "KH"});
         assertEquals(game.toString(), "Melds:\r\n" +
                 "   Player p1: {JH QH KH}");
+    }
+
+    @DisplayName("P1 play {QH QC QS}")
+    @Test
+    public void TestPlayPlaceQH_QC_QS() {
+        Game game = new Game();
+        game.addPlayer(new Player("p1"));
+        game.players.get(0).place(new String[]{"QH", "QC", "QS"});
+        assertEquals(game.toString(), "Melds:\r\n" +
+                "   Player p1: {QH QC QS}");
     }
 }
