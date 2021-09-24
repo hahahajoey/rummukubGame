002804f064a240757a4309a9b5a5eed5344fb632
Chupeng Shen <shenchupeng0807@gmail.com>
2021-09-24 15:07:22 -0400

    TEST PLACE {8H 8C 8D} {2H 3H 4H}

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest {
 
     @DisplayName("P1 plays{JH QH KH}")
     @Test
-    public void TestPlayPlaceJH_QH_KH() {
+    public void testPlayPlaceJH_QH_KH() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"JH", "QH", "KH"});
@@ -17,7 +17,7 @@ public class AppTest {
 
     @DisplayName("P1 play {QH QC QS}")
     @Test
-    public void TestPlayPlaceQH_QC_QS() {
+    public void testPlayPlaceQH_QC_QS() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"QH", "QC", "QS"});
@@ -27,7 +27,7 @@ public class AppTest {
 
     @DisplayName("P1 plays {9H 10H JH QH KH}")
     @Test
-    public void TestPlayPlace9H_10H_JH_QH_KH() {
+    public void testPlayPlace9H_10H_JH_QH_KH() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"9H", "10H", "JH", "QH", "KH"});
@@ -37,7 +37,7 @@ public class AppTest {
 
     @DisplayName("P1 plays {KH KC KS K}")
     @Test
-    public void TestPlayPlaceKH_KC_KS_K() {
+    public void testPlayPlaceKH_KC_KS_K() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"KH", "KC", "KS", "K"});
@@ -47,7 +47,7 @@ public class AppTest {
 
     @DisplayName("P1 plays {2H 3H 4H} {7S 8S 9S}")
     @Test
-    public void TestPlayPlace2H_3H_4H7S_8S_9S() {
+    public void testPlayPlace2H_3H_4H7S_8S_9S() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"2H", "3H", "4H"}, new String[]{"7S", "8S", "9S"});
@@ -57,11 +57,21 @@ public class AppTest {
 
     @DisplayName("P1 plays {2H 2S 2D} {4C 4D 4S 4H} {5D 5S 5H}")
     @Test
-    public void TestPlayPlace2H_2S_2D4C_4D_4S_4H5D_5S_5H() {
+    public void testPlayPlace2H_2S_2D4C_4D_4S_4H5D_5S_5H() {
         Game game = new Game();
         game.addPlayer(new Player("p1"));
         game.players.get(0).place(new String[]{"2H", "2S", "2D"}, new String[]{"4C", "4D", "4S", "4H"}, new String[]{"5D", "5S", "5H"});
         assertEquals(game.toString(), "Melds:\r\n" +
                 "   Player p1: {2H 2S 2D} {4C 4D 4S 4H} {5D 5S 5H}");
+    }
+
+    @DisplayName("P1 plays {8H 8C 8D} {2H 3H 4H}")
+    @Test
+    public void testPlayPlace8H_8C_8D2H_3H_4H() {
+        Game game = new Game();
+        game.addPlayer(new Player("p1"));
+        game.players.get(0).place(new String[]{"8H", "8C", "8D"}, new String[]{"2H", "3H", "4H"});
+        assertEquals(game.toString(), "Melds:\r\n" +
+                "   Player p1: {8H 8C 8D} {2H 3H 4H}");
     }
 }
