08b5c2fdcf92697c25c76723baef9e3321243842
Chupeng Shen <shenchupeng0807@gmail.com>
2021-09-24 14:02:18 -0400

    TEST PLACE {JH QH KH}

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,13 +1,17 @@
-import junit.framework.TestCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
 
-//    @DisplayName("P1 plays{ JH QH KH}")
-//    @Test
-//    void playerPlayRun() {
-//        Game game = new Game();
-//
-//    }
+    @DisplayName("P1 plays{JH QH KH}")
+    @Test
+    void TestPlayPlaceJH_QH_KH() {
+        Game game = new Game();
+        game.addPlayer(new Player("p1"));
+        game.players.get(0).place(new String[]{"JH", "QH", "KH"});
+        assertEquals(game.toString(), "Melds:\r\n" +
+                "   Player p1: {JH QH KH}");
+    }
 }
